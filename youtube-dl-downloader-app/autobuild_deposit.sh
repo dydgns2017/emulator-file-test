a2cf5905672f56815479e437ccd0ce6eab634664
Yonghoon Jung <asdf2017@naver.com>
2021-01-14 18:36:49 +0900

    add autobuild script

---
+++ youtube-dl-downloader-app/autobuild_deposit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# set variable
+devFolder=$(pwd)
+emuFolder=$HOME/AppData/Local/Android/Sdk/emulator/
+devTools=$HOME/AppData/Local/Android/Sdk/platform-tools/
+
+## emualtor execute
+# change here if you want to use a different emulator
+cd $emuFolder 
+nohup ./emulator.exe @Nexus_7_2012_API_30 &
+
+## androidjs build
+cd $devFolder
+androidjs build --force --debug
+
+## deposit
+cd $devTools
+./adb.exe -e install devFolder/dist/youtube-dl-downloader-app.apk
+
+cd $devFolder
