286eb27bad55689d8bf90b4f8625d17b640a63cb
Yonghoon Jung <asdf2017@naver.com>
2021-01-15 19:29:40 +0900

    error fix

--- youtube-dl-downloader-app/main.js
+++ youtube-dl-downloader-app/main.js
@@ -1,6 +1,6 @@
 const back = require('androidjs').back;
-const fs = require('fs')
-const youtubedl = require('youtube-dl')
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 // service define
 // download-mp34 : download mp3, mp4 files
@@ -39,18 +39,27 @@ back.on('download-mp4', (data)=>{
 	});
 });
 
-function download(data, format_num, callback){
-	let [dir, link] = data.split("|");
-	let format_ext = (format_num == 21) ? '.mp3' : '.mp4';
-	let ytl = youtubedl(link, ['--format=' + format_num], { cwd: dir + '/'});
-	let filename;
-	ytl.on('info', function(info){
-		filename = info._filename;
+// function download(data, format_num, callback){
+// 	let [dir, link] = data.split("|");
+// 	let format_ext = (format_num == 21) ? '.mp3' : '.mp4';
+// 	let ytl = youtubedl(link, ['--format=' + format_num], { cwd: dir + '/'});
+//   youtubedl.getInfo(link, (err, info)=>{
+//     if ( err ) throw err;
+//     // start download
+//     ytl.pipe(fs.createWriteStream(info._filename + format_ext));
+//   });
+// 	// end download
+// 	ytl.on('end', function() {
+// 		if ( callback != null )
+// 			callback();
+//   });
+// }
+
+back.on('test', (dir)=> {
+	ytdl("https://www.youtube.com/watch?v=QnL5P0tFkwM", {
+		format: 'mp4'
+	}).pipe(fs.createWriteStream(dir + '/myvideo.mp4'
+	)).on('finish', ()=>{
+		back.send('test', 'success');
 	});
-	// end download
-	ytl.on('end', function() {
-		if ( callback != null )
-			callback();
-	});
-	ytl.pipe(fs.createWriteStream(filename + format_ext));
-}
+});
--- youtube-dl-downloader-app/node_modules/youtube-dl/scripts/download.js
+++
@@ -1,7 +0,0 @@
-var downloader = require('../lib/downloader')
-
-downloader().then((message) => {
-  console.log(message);
-}).catch((err) => {
-  console.error(err)
-});
